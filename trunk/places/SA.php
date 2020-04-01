ddcfbd0e0f9c3e40135ed14a82e93b5eed733c21
HassanBAZtechcampus <48813709+HassanBAZtechcampus@users.noreply.github.com>
2020-04-01 05:48:21 +0300

    Create SA.php

---
+++ trunk/places/SA.php
@@ -0,0 +1,205 @@
+<?php			
+			
+/**			
+ * Saudi Arabia places			
+ *			
+ * @author   Kingsley Ochu <ochu.kc@gmail.com>			
+ * @version  1.0.0			
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License			
+ * Credits: https://donjajo.com/ngstateslga-nigerian-36-states-local-government-areas-php-class/			
+ */			
+global $places;			
+			
+$places['SA'] = array(			
+			 'AS' => array(
+			__('Abha',  'states-cities-and-places-for-woocommerce'),
+			__('Khamis-Mushait',  'states-cities-and-places-for-woocommerce'),
+			__('Bisha',  'states-cities-and-places-for-woocommerce'),
+			__('Al-Namas',  'states-cities-and-places-for-woocommerce'),
+			__('Muhayil',  'states-cities-and-places-for-woocommerce'),
+			__('Dhahran Al Janub',  'states-cities-and-places-for-woocommerce'),
+			__('Tathlith',  'states-cities-and-places-for-woocommerce'),
+			__('Sarat Abidah',  'states-cities-and-places-for-woocommerce'),
+			__('Rojal',  'states-cities-and-places-for-woocommerce'),
+			__('Balqarn',  'states-cities-and-places-for-woocommerce'),
+			__('Ahad Rafidah',  'states-cities-and-places-for-woocommerce'),
+			__('Al Majaridah',  'states-cities-and-places-for-woocommerce'),
+			__('albarik',  'states-cities-and-places-for-woocommerce'),
+			__('bariq',  'states-cities-and-places-for-woocommerce'),
+			__('tanawma',  'states-cities-and-places-for-woocommerce'),
+			__('trib',  'states-cities-and-places-for-woocommerce'),
+			__('alharija',  'states-cities-and-places-for-woocommerce'),
+		),	
+			'RH' => array(
+			__('Riyadh',  'states-cities-and-places-for-woocommerce'),
+			__('Diriyah',  'states-cities-and-places-for-woocommerce'),
+			__('Al-Kharj',  'states-cities-and-places-for-woocommerce'),
+			__('Dawadmi',  'states-cities-and-places-for-woocommerce'),
+			__('Al Majmaah',  'states-cities-and-places-for-woocommerce'),
+			__('Al-Quwayiyah',  'states-cities-and-places-for-woocommerce'),
+			__('Al Aflaj',  'states-cities-and-places-for-woocommerce'),
+			__('Wadi ad-Dawasir',  'states-cities-and-places-for-woocommerce'),
+			__('Al Zulfi',  'states-cities-and-places-for-woocommerce'),
+			__('Shuqrah',  'states-cities-and-places-for-woocommerce'),
+			__('Hotat Bani Tamim',  'states-cities-and-places-for-woocommerce'),
+			__('Afif',  'states-cities-and-places-for-woocommerce'),
+			__('Al-Ghat',  'states-cities-and-places-for-woocommerce'),
+			__('As Sulayyil',  'states-cities-and-places-for-woocommerce'),
+			__('Dhurma',  'states-cities-and-places-for-woocommerce'),
+			__('Al-Muzahmiyya',  'states-cities-and-places-for-woocommerce'),
+			__('Rumah',  'states-cities-and-places-for-woocommerce'),
+			__('Thadig',  'states-cities-and-places-for-woocommerce'),
+			__('Huraymila',  'states-cities-and-places-for-woocommerce'),
+			__('Al Hariq',  'states-cities-and-places-for-woocommerce'),
+			__('marrat',  'states-cities-and-places-for-woocommerce'),
+			__('alriyn',  'states-cities-and-places-for-woocommerce'),
+		),	
+			 'EN' => array(
+			__('Dammam',  'states-cities-and-places-for-woocommerce'),
+			__('Al Ahsa',  'states-cities-and-places-for-woocommerce'),
+			__('Hafar Al-Batin',  'states-cities-and-places-for-woocommerce'),
+			__('Jubail',  'states-cities-and-places-for-woocommerce'),
+			__('Qatif',  'states-cities-and-places-for-woocommerce'),
+			__('Khobar',  'states-cities-and-places-for-woocommerce'),
+			__('Khafji',  'states-cities-and-places-for-woocommerce'),
+			__('Ras Tanura',  'states-cities-and-places-for-woocommerce'),
+			__('Abqaiq',  'states-cities-and-places-for-woocommerce'),
+			__('Al Nairyah',  'states-cities-and-places-for-woocommerce'),
+			__('Qaryat al-Ulya',  'states-cities-and-places-for-woocommerce'),
+			__('aledyd',  'states-cities-and-places-for-woocommerce'),
+			
+		),	
+			
+			'HL' => array(
+			__('Hail',  'states-cities-and-places-for-woocommerce'),
+			__('Baqaa',  'states-cities-and-places-for-woocommerce'),
+			__('Al Khazaiah',  'states-cities-and-places-for-woocommerce'),
+			__('Al Shinan',  'states-cities-and-places-for-woocommerce'),
+			__('alhayit',  'states-cities-and-places-for-woocommerce'),
+			__('alsalimi',  'states-cities-and-places-for-woocommerce'),
+			__('alshamli',  'states-cities-and-places-for-woocommerce'),
+			__('mawqiq',  'states-cities-and-places-for-woocommerce'),
+			__('samira',  'states-cities-and-places-for-woocommerce'),
+			
+		),	
+			
+			'JF' => array(
+			__('Sakakah',  'states-cities-and-places-for-woocommerce'),
+			__('Qurayyat',  'states-cities-and-places-for-woocommerce'),
+			__('Dumat al-Jandal',  'states-cities-and-places-for-woocommerce'),
+			__('tabirajl',  'states-cities-and-places-for-woocommerce'),
+			
+		),	
+			
+			'JN' => array(
+			__('Jizan',  'states-cities-and-places-for-woocommerce'),
+			__('Sabya',  'states-cities-and-places-for-woocommerce'),
+			__('Abu `Arish',  'states-cities-and-places-for-woocommerce'),
+			__('Samtah',  'states-cities-and-places-for-woocommerce'),
+			__('Baish',  'states-cities-and-places-for-woocommerce'),
+			__('Al Darb',  'states-cities-and-places-for-woocommerce'),
+			__('Al Harth',  'states-cities-and-places-for-woocommerce'),
+			__('Damad',  'states-cities-and-places-for-woocommerce'),
+			__('Al Reeth',  'states-cities-and-places-for-woocommerce'),
+			__('Farasan',  'states-cities-and-places-for-woocommerce'),
+			__('Al Dayer',  'states-cities-and-places-for-woocommerce'),
+			__('Al Aridhah',  'states-cities-and-places-for-woocommerce'),
+			__('Ahad al Masarihah',  'states-cities-and-places-for-woocommerce'),
+			__('Al Edabi',  'states-cities-and-places-for-woocommerce'),
+			__('fayfa',  'states-cities-and-places-for-woocommerce'),
+			__('altwal',  'states-cities-and-places-for-woocommerce'),
+			__('harub',  'states-cities-and-places-for-woocommerce'),
+			
+		),	
+			
+			'NJ' => array(
+			__('Najran',  'states-cities-and-places-for-woocommerce'),
+			__('Sharurah',  'states-cities-and-places-for-woocommerce'),
+			__('Hubuna',  'states-cities-and-places-for-woocommerce'),
+			__('Badr Al Janub',  'states-cities-and-places-for-woocommerce'),
+			__('Yadamah',  'states-cities-and-places-for-woocommerce'),
+			__('Thar',  'states-cities-and-places-for-woocommerce'),
+			__('Khubash',  'states-cities-and-places-for-woocommerce'),
+			__('Al Kharkhir',  'states-cities-and-places-for-woocommerce'),
+			
+		),	
+			'QM' => array(
+			__('Buraidah',  'states-cities-and-places-for-woocommerce'),
+			__('Unaizah',  'states-cities-and-places-for-woocommerce'),
+			__('Ar Rass',  'states-cities-and-places-for-woocommerce'),
+			__('Al Mithnab',  'states-cities-and-places-for-woocommerce'),
+			__('Al Bukayriyah',  'states-cities-and-places-for-woocommerce'),
+			__('Al Badayea',  'states-cities-and-places-for-woocommerce'),
+			__('Asyah',  'states-cities-and-places-for-woocommerce'),
+			__('Al Nabhaniyah',  'states-cities-and-places-for-woocommerce'),
+			__('Al Shimasiyah',  'states-cities-and-places-for-woocommerce'),
+			__('Uyun AlJiwa',  'states-cities-and-places-for-woocommerce'),
+			__('Riyadh Al Khabra',  'states-cities-and-places-for-woocommerce'),
+			__('euqlat alsuqur',  'states-cities-and-places-for-woocommerce'),
+			__('duriya',  'states-cities-and-places-for-woocommerce'),
+			
+		),	
+			'TB' => array(
+			__('Tabuk',  'states-cities-and-places-for-woocommerce'),
+			__('Al Wajh',  'states-cities-and-places-for-woocommerce'),
+			__('Duba',  'states-cities-and-places-for-woocommerce'),
+			__('Tayma',  'states-cities-and-places-for-woocommerce'),
+			__('Umluj',  'states-cities-and-places-for-woocommerce'),
+			__('Haql',  'states-cities-and-places-for-woocommerce'),
+			__('albadae',  'states-cities-and-places-for-woocommerce'),
+			
+		),	
+			'MK' => array(
+			__('Mecca',  'states-cities-and-places-for-woocommerce'),
+			__('Jeddah',  'states-cities-and-places-for-woocommerce'),
+			__('Taif',  'states-cities-and-places-for-woocommerce'),
+			__('Al Qunfudhah',  'states-cities-and-places-for-woocommerce'),
+			__('Al Lith',  'states-cities-and-places-for-woocommerce'),
+			__('Rabigh',  'states-cities-and-places-for-woocommerce'),
+			__('Khulays',  'states-cities-and-places-for-woocommerce'),
+			__('Al Khurmah',  'states-cities-and-places-for-woocommerce'),
+			__('Ranyah',  'states-cities-and-places-for-woocommerce'),
+			__('Turubah',  'states-cities-and-places-for-woocommerce'),
+			__('Al Jumum',  'states-cities-and-places-for-woocommerce'),
+			__('Al Kamil',  'states-cities-and-places-for-woocommerce'),
+			__('almawiyuh',  'states-cities-and-places-for-woocommerce'),
+			__('maysan',  'states-cities-and-places-for-woocommerce'),
+			__('aduma',  'states-cities-and-places-for-woocommerce'),
+			__('alerdiat',  'states-cities-and-places-for-woocommerce'),
+			__('bahratan',  'states-cities-and-places-for-woocommerce'),
+			
+		),	
+			'BH' => array(
+			__('Al Bahah',  'states-cities-and-places-for-woocommerce'),
+			__('Baljurashi',  'states-cities-and-places-for-woocommerce'),
+			__('Al Mandaq',  'states-cities-and-places-for-woocommerce'),
+			__('Al Makhwah',  'states-cities-and-places-for-woocommerce'),
+			__('Qilwah',  'states-cities-and-places-for-woocommerce'),
+			__('Al Aqiq',  'states-cities-and-places-for-woocommerce'),
+			__('Al Qara',  'states-cities-and-places-for-woocommerce'),
+			__('ghamid alzinad',  'states-cities-and-places-for-woocommerce'),
+			__('alhajra',  'states-cities-and-places-for-woocommerce'),
+			__('bani hasan',  'states-cities-and-places-for-woocommerce'),
+			
+		),	
+			'NB' => array(
+			__('Arar',  'states-cities-and-places-for-woocommerce'),
+			__('Rafha',  'states-cities-and-places-for-woocommerce'),
+			__('Turaif',  'states-cities-and-places-for-woocommerce'),
+			__('alewyqila',  'states-cities-and-places-for-woocommerce'),
+			
+		),	
+			'MD' => array(
+			__('Medina',  'states-cities-and-places-for-woocommerce'),
+			__('Yanbu',  'states-cities-and-places-for-woocommerce'),
+			__('Al-Ula',  'states-cities-and-places-for-woocommerce'),
+			__('Mahd adh Dhahab',  'states-cities-and-places-for-woocommerce'),
+			__('Al Hinakiyah',  'states-cities-and-places-for-woocommerce'),
+			__('Badr',  'states-cities-and-places-for-woocommerce'),
+			__('Khaybar',  'states-cities-and-places-for-woocommerce'),
+			__('aleays',  'states-cities-and-places-for-woocommerce'),
+			__('wadi alfare',  'states-cities-and-places-for-woocommerce'),
+			
+		)	
+			
+		);	
