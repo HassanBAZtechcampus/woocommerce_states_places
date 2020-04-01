a666f2aa8f7937a0c8a048cff752c7221b7c0206
HassanBAZtechcampus <48813709+HassanBAZtechcampus@users.noreply.github.com>
2020-04-01 05:48:03 +0300

    Create SA.php

---
+++ trunk/states/SA.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Saudi Arabia states
+ *
+ * @author   Kingsley Ochu <ochu.kc@gmail.com>
+ * @version  1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * Credits: alexasomba
+ */
+
+global $states;
+
+$states ['SA' ] = array (
+ 'AS' => __('Asir Region', 'states-cities-and-places-for-woocommerce'),
+ 'RH' => __('Riyadh Region' , 'states-cities-and-places-for-woocommerce'),
+ 'EN' => __('Eastern Region' , 'states-cities-and-places-for-woocommerce'),
+ 'HL' => __('Hail Region' , 'states-cities-and-places-for-woocommerce'),
+ 'JF' => __('Al-Jouf Region' , 'states-cities-and-places-for-woocommerce'),
+ 'JN' => __('Jazan Region' , 'states-cities-and-places-for-woocommerce'),
+ 'NJ' => __('Najran Region' , 'states-cities-and-places-for-woocommerce'),
+ 'QM' => __('Qassim Region' , 'states-cities-and-places-for-woocommerce'),
+ 'TB' => __('Tabuk Region' , 'states-cities-and-places-for-woocommerce'),
+ 'MK' => __('Makkah Region' , 'states-cities-and-places-for-woocommerce'),
+ 'BH' => __('Al Bahah Region' , 'states-cities-and-places-for-woocommerce'),
+ 'NB' => __('Northern Border Region' , 'states-cities-and-places-for-woocommerce'),
+ 'MD' => __('Madinah Region' , 'states-cities-and-places-for-woocommerce')
+	);
